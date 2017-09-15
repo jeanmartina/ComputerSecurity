0d593e7676ee43644888e70fedafaba2d3abf5b6
Guangting Yu <yugtmath@umich.edu>
2017-09-15 17:59:37 -0400

    lab 2

---
+++ Lab/Lab2.py
@@ -0,0 +1,22 @@
+# import sys
+#
+# input=sys.argv[1]
+# output=[" problems but EECS 388 ain't one"]
+#
+# if input>1:
+#     output.append(input)
+#     output.reverse()
+#     print ''.join(output)
+
+
+# Caesar cipher
+def encrypt(msg,k):
+    msg=msg.upper()
+    ciphertext=''
+    for letter in msg:
+        cipher_num=(ord(letter)-ord('A')+k)%26
+        cipher_chr=chr(cipher_num+ord('A'))
+        ciphertext+=cipher_chr
+    print('Ciohertext: '+ciphertext)
+
+encrypt('helloworldtest',5)
