38b5abdf29a96e6bbc47bb0b87d353adb7ca10aa
guangting's linux <yugtmath@umich.edu>
2017-09-23 21:32:37 -0400

    no progrss on rsa

--- Projects/Project1/rsa/demo.py
+++ Projects/Project1/rsa/demo.py
@@ -5,8 +5,8 @@ n = 0x00bbcc67e6218f02a9b5e358cf36cf1ef3ea76f32bb3645f1de2212beba2f6fd181cdc855b
 e = 3
 signature = int(open('sig').read().encode('hex'), 16)
 x = pow(signature, e, n)
-print "%0128x" % x
+print("%0128x" % x)
 
 m = hashlib.sha256()
 m.update("EECS 388 rul3z!")
-print m.hexdigest()
+print(m.hexdigest())
